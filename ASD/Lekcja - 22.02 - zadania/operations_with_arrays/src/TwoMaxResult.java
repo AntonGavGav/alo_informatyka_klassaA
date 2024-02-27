f1781a6e8f172aa61d6530cc6db0e84c4068ccb9
EventfulLynx915 <57542365+AntonGavGav@users.noreply.github.com>
2024-02-27 23:01:19 +0100

    Lekcja  - 22.02 -  zadania
    * Lekcja - 22.02 - zadania, task1, start of task2
    
    * Lekcja - 22.02 - zadania, finish task
    
    * Lekcja - 22.02 - zadania, add link

---
+++ ASD/Lekcja - 22.02 - zadania/operations_with_arrays/src/TwoMaxResult.java
@@ -0,0 +1,15 @@
+public class TwoMaxResult {
+    private int max;
+    private int max2;
+
+    public TwoMaxResult(int max, int max2){
+        this.max = max;
+        this.max2 = max2;
+    }
+    public int getMax() {
+        return max;
+    }
+    public int getMax2() {
+        return max2;
+    }
+}
